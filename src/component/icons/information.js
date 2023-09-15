22339a13066e55ea4c7193d9e131e1412da956c1
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-09-15 23:35:17 +0600

    add product from design

---
+++ src/component/icons/information.js
@@ -0,0 +1,8 @@
+"use client"
+import { Icon } from '@iconify/react';
+
+const Information = ({onClick,className}) => {
+    return <Icon onClick={()=>onClick()} className={className} icon="heroicons-outline:information-circle" />
+};
+
+export default Information;
--- src/component/ui/button.js
+++ src/component/ui/button.js
@@ -1,6 +1,8 @@
-const Button = ({ className, children, variant }) => {
+const Button = ({disabled,type, className, children, variant }) => {
   return (
     <button
+    disabled={disabled}
+    type={type}
       className={`py-2 px-3 rounded ${variant==="outline"?"text-[#8298a2] bg-transparent border border-[#8298a2] hover:bg-white":"text-white bg-[#94aeb9] hover:bg-[#8298a2]"} ${className}`}
     >
       {children}
