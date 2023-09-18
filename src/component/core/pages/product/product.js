314cd8f87b8ee520198ebf5a5f362e82c54bab2a
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-09-18 23:19:03 +0600

    modefied::addProduct

--- src/component/core/pages/product/product.js
+++ src/component/core/pages/product/product.js
@@ -5,24 +5,27 @@ import ProductMain from "./productMain/productMain";
 const Product = ({ product, loading }) => {
   return (
     <>
-        <>
-          <div className="grid md:grid-cols-7 mt-8">
-            <div className="col-span-5">
-              <ProductMain product={product} loading={loading}/>
-            </div>
-            <div className="col-span-2 ">
-              <ProductSidebar product={product} loading={loading}/>
-            </div>
+      <>
+        <div className="grid md:grid-cols-7 mt-8">
+          <div className="col-span-5">
+            <ProductMain product={product} loading={loading} />
           </div>
-          {/* description */}
-          <div className="rounded mt-4 bg-slate-50 p-6 md:p-8">
+          <div className="col-span-2 ">
+            <ProductSidebar product={product} loading={loading} />
+          </div>
+        </div>
+        {/* description */}
+        <div className="rounded mt-4 bg-slate-50 p-6 md:p-8">
           {!loading ? (
-            <p>{product?.description}</p>
-            ) : (
-              <div className="loader mt-6"></div>
-            )}
-          </div>
-        </>
+            <>
+              <h2 className="font-normal text-lg">Description</h2>
+              <p className="text-[#516067]">{product?.description}</p>
+            </>
+          ) : (
+            <div className="loader mt-6"></div>
+          )}
+        </div>
+      </>
     </>
   );
 };
