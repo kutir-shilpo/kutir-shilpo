970eef52fbbd3c82eae4ec44ebf3f15f1d4085c7
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-09-21 20:04:45 +0600

    fixed:: cart-item error

--- src/app/(dashboard)/cart-items/page.js
+++ src/app/(dashboard)/cart-items/page.js
@@ -1,8 +1,6 @@
+"use client"
 import CartItems from '@/component/core/pages/dashboard/mainContain/cartItems';
 
-export const metadata = {
-    title: "Kutir Shilpo-cart-items",
-  };
 const Cartitem = () => {
     return (
         <>
