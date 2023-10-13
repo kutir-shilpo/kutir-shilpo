8ab32772ca3301e858f1cfe766812f7af6b91a8e
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-10-13 22:15:00 +0600

    use cart error fixed

---
+++ src/component/core/pages/Home/facebookMsg/facebookMsg.js
@@ -0,0 +1,11 @@
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
+const FacebookMsg = () => {
+    return (
+        <FacebookProvider appId="123456789" chatSupport>
+        <CustomChat pageId="123456789" minimized={false}/>
+      </FacebookProvider>   
+    );
+};
+
+export default FacebookMsg;
